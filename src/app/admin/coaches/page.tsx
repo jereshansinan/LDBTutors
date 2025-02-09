bbe25c9050fa6edbaea5904a9dd332812e009888
ST10033851 <ST10033851@vcconnect.edu.za>
2025-02-09 16:56:30 +0200

    Updated pages

---
+++ src/app/admin/coaches/page.tsx
@@ -0,0 +1,5 @@
+export default function CoachesPage() {
+    return (
+      <p>Booking page</p>
+    )
+  }
---
+++ src/app/coach/check-in/page.tsx
@@ -0,0 +1,5 @@
+export default function CheckInPage() {
+    return (
+      <p>Checkin page</p>
+    )
+  }
