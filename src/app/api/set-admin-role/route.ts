164e400cf4fbf57f750c52ff2376934440fee2f6
ST10033851 <ST10033851@vcconnect.edu.za>
2025-04-23 13:42:33 +0200

    Added role Declaration

---
+++ src/app/api/set-admin-role/route.ts
@@ -0,0 +1,37 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+export async function POST() {
+  const adminUserIds = [
+    "user_2w81Fmif3YYORhssimF4qmnFMn9",
+    "user_2w819a9LMFdPNi81JZooZ4myxdH",
+    "user_2w7wMpeZgPuDnNsIL7qlazWFXi5",
+  ];
+
+  try {
+    const client = await clerkClient();
+
+    for (const userId of adminUserIds) {
+      await client.users.updateUserMetadata(userId, {
+        publicMetadata: {
+          role: "admin",
+        },
+      });
+    }
+    return NextResponse.json(
+      { message: "admins registered successfully!" },
+      { status: 200 }
+    );
+  } catch (clerkError) {
+    // Fixed error handling
+    console.error("Error creating user in Clerk:", clerkError);
+    console.error("Clerk Response:", clerkError || clerkError);
+    return NextResponse.json(
+      {
+        error: "Clerk user role declare failed",
+        details: clerkError || clerkError,
+      },
+      { status: 422 }
+    );
+  }
+}
