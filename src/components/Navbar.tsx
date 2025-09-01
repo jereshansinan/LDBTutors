a873f682dafc3eba3e3fcab1b50e7f9cc3f73fb7
Jereshan <jereshan.sinan11@gmail.com>
2025-09-01 22:01:17 +0200

    Updated home page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -54,13 +54,6 @@ export default function Navbar() {
   const [showModal, setShowModal] = useState(false);
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
-  const toggleLanguage = () => {
-    const newLanguage = currentLanguage === "en" ? "fr" : "en";
-    setCurrentLanguage(newLanguage);
-    localStorage.setItem("language", newLanguage);
-    window.location.reload();
-  };
-
   useEffect(() => {
     const handleScroll = () => {
       setIsScrolled(window.scrollY > 50);
@@ -155,23 +148,6 @@ export default function Navbar() {
               {/* Language Toggle Switch for Desktop */}
               <div className="flex items-center">
                 {/* Language Toggle Switch */}
-                <label className="inline-flex items-center cursor-pointer">
-                  <input
-                    type="checkbox"
-                    value=""
-                    className="sr-only peer"
-                    checked={currentLanguage === "fr"}
-                    onChange={toggleLanguage}
-                  />
-                  <div className="relative w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-[#75E379] dark:peer-focus:ring-[#75E379] rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-[#75E379] dark:peer-checked:bg-[#75E379]"></div>
-                  <span
-                    className={`ms-3 font-bold text-xl ${
-                      isScrolled ? "text-black" : "text-white"
-                    }`}
-                  >
-                    {currentLanguage === "en" ? "Français" : "English"}
-                  </span>
-                </label>
               </div>
 
               {isSignedIn ? (
